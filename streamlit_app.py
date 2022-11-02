975934f2c48fe47f1850988c488c969e79103ae2
Chinmay Das <chinmay98@gmail.com>
2022-11-02 17:06:59 -0400

    Update streamlit_app.py
    Added the query to pull the color selection and display the data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,21 @@ import pandas
 
 streamlit.title("Zena\'s Amazing Atheleisure Catalog")
 
+#Snowflake Connection 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-
 my_cur = my_cnx.cursor()
 
-my_cur.execute("select current_user(), current_account(), current_region()")
+#Run a snowflake query and put in in a variable called my_catalog
+my_cur.execute("select color_or_style from catalog_for_website")
+my_catalog = my_cur.fetchall()
 
-my_data_row = my_cur.fetchone()
+#Put to data into a Data Frame 
+df = Pandas.DataFrame(my_catalog)
 
-streamlit.text("Hello from Snowflake")
-streamlit.text(my_data_row)
+#Temp:Write the dataframe to the screen to check the data frame is working 
+streamlit.text(df)
+
+#streamlit.text("Hello from Snowflake")
+#streamlit.text(my_data_row)
 
 
